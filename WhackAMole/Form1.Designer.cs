d8666631ee5be767d7595408a4f384eb2bd8f476
stefeo <stefeo9@gmail.com>
2016-01-20 11:35:24 -0800

    lol

--- WhackAMole/Form1.Designer.cs
+++ WhackAMole/Form1.Designer.cs
@@ -29,19 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(251, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 66);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(340, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 66);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(251, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 66);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(162, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 66);
+            this.label4.TabIndex = 3;
             // 
             // gameTimer
             // 
             this.gameTimer.Interval = 17;
-            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -50,6 +94,10 @@
 
         #endregion
 
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer gameTimer;
     }
 }
--- WhackAMole/Form1.cs
+++ WhackAMole/Form1.cs
@@ -16,46 +16,166 @@ namespace WhackAMole
         int mole;
         int moleChance = 60;
         int moleSpot;
+        int moleVariation;
         int plr1Score;
         int plr2Score;
+        int moleAmount;
+        int plr1Miss;
+        int plr2Miss;
+
+        bool mole0 = false;
+        bool mole1 = false;
+        bool mole2 = false;
+        bool mole3 = false;
+        bool plr1Enabled = true;
+        bool plr2Enabled = true;
 
         Random rndNum = new Random();
 
         public Form1()
         {
             InitializeComponent();
+            gameTimer.Start();
         }
-        private void gameTimer_Tick(object sender, EventArgs e)
-        {
-            mole = rndNum.Next(0, moleChance);
-            if (mole == 1)
-            {
-                moleSpot = rndNum.Next(0, 4);
-                SpawnMole(moleSpot);
-            }
-        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        { 
+             //check to see if a key is pressed and set is KeyDown value to true if it has 
+             switch (e.KeyCode) 
+             { 
+                 case Keys.M:
+                    if (mole1 == true)
+                    {
+                        mole1 = false;
+                        plr1Score++;
+                    }
+                    else
+                    {
+                        plr1Miss++;
+                    }
+                     break; 
+                 case Keys.N:
+                    if (mole0 == true)
+                    {
+                        mole0 = false;
+                        plr1Score++;
+                    }
+                    else
+                    {
+                        plr1Miss++;
+                    }
+                    break; 
+                 case Keys.B:
+                    if (mole3 == true)
+                    {
+                        mole3 = false;
+                        plr1Score++;
+                    }
+                    else
+                    {
+                        plr1Miss++;
+                    }
+                    break; 
+                 case Keys.Space:
+                    if (mole2 == true)
+                    {
+                        mole2 = false;
+                        plr1Score++;
+                    }
+                    else
+                    {
+                        plr1Miss++;
+                    }
+                    break;
+                case Keys.V:
+                    break;
+                case Keys.C:
+                    break;
+                case Keys.X:
+                    break;
+                case Keys.Z:
+                    break;
+                 default: 
+                     break; 
+             }  
+ 
+         }
+
+
         public void SpawnMole(int moleSpot)
         {
             switch (moleSpot)
             {
                 case 0:
-                    MolePop(0);
+                    if (mole0 == false)
+                    {
+                        label1.BackColor = Color.Red;
+                        mole0 = true;
+                        //spawn mole in place 0
+                    }
                     break;
                 case 1:
-                    MolePop(1);
+                    if (mole1 == false)
+                    {
+                        label2.BackColor = Color.Red;
+                        mole1 = true;
+                        //spawn mole in place 1
+                    }
                     break;
                 case 2:
-                    MolePop(2);
+                    if (mole2 == false)
+                    {
+                        label3.BackColor = Color.Red;
+                        mole2 = true;
+                        //spawn mole in place 2
+                    }
                     break;
                 case 3:
-                    MolePop(3);
+                    if (mole3 == false)
+                    {
+                        label4.BackColor = Color.Red;
+                        mole3 = true;
+                        //spawn mole in place 3
+                    }
                     break;
             }
         }
-        public void MolePop(int moleValue)
+        public void SpawnMole2(int moleSpot, int moleVariation)
         {
-            switch (moleValue)
+            SpawnMole(moleSpot);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (mole0 == false)
             {
+                label1.BackColor = Color.Black;
+            }
+            if (mole1 == false)
+            {
+                label2.BackColor = Color.Black;
+            }
+            if (mole2 == false)
+            {
+                label3.BackColor = Color.Black;
+            }
+            if (mole3 == false)
+            {
+                label4.BackColor = Color.Black;
+            }
+            mole = rndNum.Next(0, moleChance);
+            if (mole == 1)
+            {
+                moleAmount++;
+                moleSpot = rndNum.Next(0, 5);
+                SpawnMole(moleSpot);
+            }
+            if (mole == 2)
+            {
+                moleAmount += 2;
+                moleSpot = rndNum.Next(0, 4);
+                moleVariation = rndNum.Next(0, 3);
 
             }
         }

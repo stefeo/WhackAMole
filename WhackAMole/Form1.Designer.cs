8d89f1defcf7c947541ca3c04bd7182c81732714
stefeo <stefeo9@gmail.com>
2016-01-20 10:26:10 -0800

    Initial Commit

---
+++ WhackAMole/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WhackAMole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 17;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ WhackAMole/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhackAMole
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        int mole;
+        int moleChance = 60;
+        int moleSpot;
+        int plr1Score;
+        int plr2Score;
+
+        Random rndNum = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            mole = rndNum.Next(0, moleChance);
+            if (mole == 1)
+            {
+                moleSpot = rndNum.Next(0, 4);
+                SpawnMole(moleSpot);
+            }
+        }
+        public void SpawnMole(int moleSpot)
+        {
+            switch (moleSpot)
+            {
+                case 0:
+                    MolePop(0);
+                    break;
+                case 1:
+                    MolePop(1);
+                    break;
+                case 2:
+                    MolePop(2);
+                    break;
+                case 3:
+                    MolePop(3);
+                    break;
+            }
+        }
+        public void MolePop(int moleValue)
+        {
+            switch (moleValue)
+            {
+
+            }
+        }
+    }
+}
